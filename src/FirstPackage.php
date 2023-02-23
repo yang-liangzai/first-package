8eb01786a0d9234e40d8b3e149a1097ca7788fe8
toby <toby@tranhom.com>
2023-02-23 18:29:36 +0800

    submit .omposer.json

---
+++ src/FirstPackage.php
@@ -0,0 +1,15 @@
+<?php
+namespace yang-bg\FirstPackage;
+
+/**
+ * 
+ */
+class FirstPackage extends AnotherClass
+{
+    
+    public function get_time(){
+        return date('Y-m-d H:i:s');
+    }
+}
+
+?>
